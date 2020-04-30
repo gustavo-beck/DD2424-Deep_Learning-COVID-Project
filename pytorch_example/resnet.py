16db80f093f5168b4030c0ebfb2ee023709b5172
theo2021 <theo2021@gmail.com>
2020-04-30 19:25:49 +0300

    simple covnet

--- pytorch_example/resnet.py
+++ pytorch_example/resnet.py
@@ -8,6 +8,7 @@ import torch
 import os
 import time
 import random
+from torch import nn
 print('libraries alright')
 path="/home/theodor/Desktop/dataset/"
 img_name=[]
@@ -21,8 +22,9 @@ for root, directories, files in os.walk(path):
 # RandomAffine does a serries of transformations as explained here https://www.mathworks.com/discovery/affine-transformation.html
 # with those transformations, the network will be able to handle distorted input
 # torchvision.transforms.ToTensor() converts image to tensor input
-train_Aug = torchvision.transforms.Compose([torchvision.transforms.Resize((224, 224)), torchvision.transforms.RandomRotation((-20, 20)), torchvision.transforms.RandomAffine(0, translate=None, scale=[0.7, 1.3], shear=None, resample=False, fillcolor=0), torchvision.transforms.ToTensor()])
-test_Aug = torchvision.transforms.Compose([torchvision.transforms.Resize((224, 224)), torchvision.transforms.ToTensor()]) # validation shouldn't be distorted, only resized
+images_size = 224 # 224
+train_Aug = torchvision.transforms.Compose([torchvision.transforms.Resize((images_size, images_size)), torchvision.transforms.RandomRotation((-20, 20)), torchvision.transforms.RandomAffine(0, translate=None, scale=[0.7, 1.3], shear=None, resample=False, fillcolor=0), torchvision.transforms.ToTensor()])
+test_Aug = torchvision.transforms.Compose([torchvision.transforms.Resize((images_size, images_size)), torchvision.transforms.ToTensor()]) # validation shouldn't be distorted, only resized
 
 # not neccessary to define it, but it helps organize the input
 # class is a child of torch.utils.data.Dataset
@@ -57,26 +59,53 @@ train_set = img_name[:train_nums]
 train_labels= label[:train_nums]
 test_set = img_name[train_nums:]
 test_labels= label[train_nums:]
-
-
 train_set = CustomDatasetFromImages(train_set, train_labels, transforms=train_Aug)
 test_set = CustomDatasetFromImages(test_set, test_labels, transforms=test_Aug)
 # The loader is used to slpit the input and validation to batches, it returns arrays with the input in batches
 trainloader = torch.utils.data.DataLoader(train_set, batch_size=64, num_workers=2,shuffle=True) 
 testloader = torch.utils.data.DataLoader(test_set, batch_size=64, num_workers=2, shuffle=False)
+print(train_set[0][0], train_set[0][1].sum(), train_set[0][2])
+print(train_set[0][0], train_set[0][1].sum(), train_set[0][2])
 
+
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=4, stride=4)) # 2x2 --> 1 2h->1h and 2w->1w : 28x28 --> 14x14
+            
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=4, stride=4)) #2x2 --> 1 2h->1h and 2w->1w : 14x14 --> 7x7
+        self.drop_out = nn.Dropout()
+        self.fc1 = nn.Linear(14 * 14 * 32, 2)
+        #self.fc2 = nn.Linear(1000, 10)
+    def forward(self, x):
+        layer1 = self.layer1(x)
+        layer2 = self.layer2(layer1)
+        ## vectorizing the input matrix
+        layer2_vec = layer2.view(-1, 32*14*14)
+        return torch.nn.Softmax(1)(self.fc1(self.drop_out(layer2_vec)))
 
 model = torchvision.models.resnet18(pretrained=True)
+# model.connection1 = torch.nn.Linear(512, 220)
+
 model.fc = torch.nn.Linear(512, 2) # fc stands for fully connected layer 512 input 2 the output
 model = torch.nn.Sequential(model, torch.nn.Softmax(1)) # We do a sequential pipeline
 #test_model = torch.nn.Sequential(torch.nn.Conv2d(1, 20, 5), )
+cov_model = ConvNet()
+model = cov_model
 criterion = torch.nn.CrossEntropyLoss()
+#optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0005)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0005)
 # scheduler is used to adjust the learning rate,
 # this oparticular scheduler uses the validation accuracy to adjust the learning rate,
 # other schedulers dont require the validation accuracy
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=10, eps=1e-06)
-
 # in pytorch training isn't that easy as keras, but we have additional flexibility
 epochs = 10
 for epoch in range(epochs):
