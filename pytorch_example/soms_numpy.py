a1a73ea71b7e209c242b05530841cd478951f7c8
Gustavo Beck <teodoro.beck@gmail.com>
2020-05-07 00:14:33 +0200

    SOMs coded in numpy ready to go

--- pytorch_example/soms_numpy.py
+++ pytorch_example/soms_numpy.py
@@ -3,6 +3,7 @@ import pickle
 import cv2
 from tqdm import tqdm
 from matplotlib import pyplot as plt
+from matplotlib.pyplot import cm
 
 class myData:
     def __init__(self,data,labels):
@@ -13,12 +14,15 @@ def distance(grid, sample):
     return np.sum((grid - sample)**2, axis=2)
 
 class SOM:
-    def __init__(self, inputs, grid_dim, max_learning_rate = 0.2, min_learning_rate = 0.01, radious = 15): # input should be flattened
-        np.random.seed(10)
+    def __init__(self, inputs, grid_width, grid_height, max_learning_rate = 0.8, min_learning_rate = 0.05, radious = 10): # input should be flattened
+        np.random.seed(42)
         mean = np.mean(inputs, axis=0)
         std = np.std(inputs, axis=0)
-        self.grid_dim = grid_dim
-        self.grid = np.random.randn(grid_dim, grid_dim, mean.shape[0])*std*0.1 + mean
+        self.grid_height = grid_height
+        self.grid_width = grid_width
+        #self.grid = np.random.randn(grid_dim, grid_dim, mean.shape[0])*std*0.5 + mean
+        # Try different initialization
+        self.grid = np.random.normal(mean, std*0.25, (grid_width, grid_height, mean.shape[0]))
         self.max_learning_rate = max_learning_rate
         self.min_learning_rate = min_learning_rate
         self.radious = radious
@@ -33,13 +37,13 @@ class SOM:
     def update_grid(self, samples, lr, current_radious):
         winners = self.find_winner(samples)
         for (win_x, win_y), sample in zip(winners, samples):
-            for candidate_x, candidate_y in np.ndindex(self.grid_dim, self.grid_dim):
+            for candidate_x, candidate_y in np.ndindex(self.grid_width, self.grid_height):
                 distance_from_winner = (candidate_x - win_x)**2 + (candidate_y - win_y)**2
                 if distance_from_winner <= current_radious**2:
                     self.grid[candidate_x, candidate_y] += lr*(sample - self.grid[candidate_x, candidate_y])*np.exp(-distance_from_winner / 2*current_radious**2)
             
     def train(self, inputs, epochs, batch_size):
-        tau = epochs**2/(np.log(2*self.radious))
+        tau = epochs**2/(np.log(4*self.radious))
         for epoch in tqdm(range(epochs)): 
             current_radious = int(self.radious*np.exp(-(epoch)**2/tau))
             lr = self.max_learning_rate - (self.max_learning_rate - self.min_learning_rate)*epoch/epochs
@@ -47,8 +51,9 @@ class SOM:
                 batch = inputs[i:i+batch_size]
                 self.update_grid(batch, lr, current_radious)
                 
-                
-path_for_soms = r'/home/theodor/Git_Projects/DD2424-Deep_Learning-COVID-Project/pytorch_example/dataset/soms/balanched_data.pickle'
+
+dictionary = {0:'Atelectasis', 1:'Cardiomegaly', 2:'Consolidation', 3:'Edema', 4:'Effusion', 5:'Emphysema', 6:'Fibrosis', 7:'Hernia', 8:'Infiltration', 9:'Mass', 10:'COVID-19', 11:'Nodule', 12:'Pleural_Thickening', 13:'Pneumonia', 14:'Pneumothorax'}      
+path_for_soms = r'C:\Users\teodo\OneDrive\Desktop\KTH\4th_Period\Deep_Learning\Project\DD2424-Deep_Learning-COVID-Project\pytorch_example\balanced_data.pickle'
 soms_dataset = pickle.load(open(path_for_soms, 'rb'))
 indexes = np.random.permutation(soms_dataset.labels.shape[0])
 soms_dataset.data = soms_dataset.data[indexes]
@@ -56,8 +61,8 @@ soms_dataset.labels = soms_dataset.labels[indexes]
 
 def reshapeImages(images):
     # setting dim of the resize
-    height = 32
-    width = 32
+    height = 64
+    width = 64
     dim = (width, height)
     # reshaping
     reshaped = [cv2.resize(np.array(image), dim, interpolation=cv2.INTER_LINEAR) for image in images]
@@ -66,16 +71,22 @@ def reshapeImages(images):
 
 resized_images = reshapeImages(soms_dataset.data)
 flattened = resized_images.reshape(resized_images.shape[0], -1)
-
+# Normalize inputs
+'''
+mean_flat = np.mean(flattened, axis=0)
+std_flat = np.std(flattened, axis=0)
+normal_flat = (flattened - mean_flat) / std_flat
+'''
 # Train SOM
-grid_dim = 20
-epochs = 1
-batch_size = 75
-som = SOM(flattened, grid_dim)
+grid_height = 10
+grid_width = 10
+epochs = 20
+batch_size = 1
+som = SOM(flattened, grid_width, grid_height)
 som.train(flattened, epochs, batch_size)
 
 # Plot the clusters, This is going to take much time
-ranking = np.zeros((grid_dim, grid_dim, 15))
+ranking = np.zeros((grid_width, grid_height, 15))
 for sample, label in tqdm(zip(flattened, soms_dataset.labels)):
     pos_x, pos_y = som.find_winner([sample])[0]
     ranking[int(pos_x), int(pos_y), label] += 1
@@ -84,10 +95,26 @@ number_of_points = np.sum(ranking, axis=2)
 percentage_of_winner = np.max(ranking, axis=2)/number_of_points
 number_of_points /= np.sum(number_of_points)
 
-plt.figure(figsize=(8,8))
-for i, j in np.ndindex(grid_dim, grid_dim):
-    plt.scatter(i,j, c='g', alpha=0.5, s=number_of_points[i,j]*900)
-    plt.scatter(i,j, c='b', alpha=0.5, s=percentage_of_winner[i,j]*40)
-    plt.text(i, j, ranking_winners[i,j], ha='center', va='center', 
-        bbox=dict(facecolor='white', alpha=0.5, lw=0))
+
+n = len(dictionary)
+color= cm.rainbow(np.linspace(0,1,n))
+c=np.array(color)
+color_labels = np.zeros(len(dictionary))
+fig, ax = plt.subplots()
+for i, j in np.ndindex(grid_width, grid_height):
+    label = dictionary[ranking_winners[i,j]]
+    if color_labels[ranking_winners[i,j]] == 0: 
+        plt.scatter(i,j, c=c[ranking_winners[i,j]], alpha=0.7, s=number_of_points[i,j]*1e4, edgecolors='none', label=label)
+        color_labels[ranking_winners[i,j]] += 1
+    else:
+        plt.scatter(i,j, c=c[ranking_winners[i,j]], alpha=0.7, s=number_of_points[i,j]*1e4, edgecolors='none')
+
+
+lgnd = plt.legend(markerscale=1, loc='center left', bbox_to_anchor=(1, 0.5), scatterpoints=1, fontsize=10)
+for handle in lgnd.legendHandles:
+    handle.set_sizes([30.0])
+
+# show the figure
+fig.tight_layout()
 plt.show()
+
