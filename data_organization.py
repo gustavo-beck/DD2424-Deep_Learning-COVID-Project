9f8eae6db3101ff7c17323dd0ca4c145d42652d9
Gustavo Beck <teodoro.beck@gmail.com>
2020-05-07 18:19:04 +0200

    Multi Hot Encoding data structure

--- data_organization.py
+++ data_organization.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-PATH = "~/Git_Projects/DD2424-Deep_Learning-COVID-Project/xray_dataset/"
+PATH = r"xray_dataset/"
 df = pd.read_csv(PATH + "Data_Entry_2017.csv")
 train_val = pd.read_csv(PATH + "train_val_list.txt", names = ['Image Index'])
 train_val['Dataset'] = 'train'
@@ -17,22 +17,19 @@ for i, sample in tqdm(df.iterrows()):
             findings_dict[finding] = len(findings_dict.keys())
     
 cols = list(findings_dict.keys())
-cols.append('Dataset')
 # cols.extend(findings_dict.keys())
 
 new_df = pd.DataFrame(df['Image Index'])
 for col in cols:
     new_df[col] = 0
 
-it = 0
+new_df = new_df.set_index('Image Index')
+datasets = datasets.set_index('Image Index')
+new_df  = pd.merge(datasets, new_df, on="Image Index", how="right")
+
 for i, sample in tqdm(df.iterrows()):
     findings = sample['Finding Labels'].split('|')
     for finding in findings:
         new_df[finding].iloc[i] = 1
-        new_df['Dataset'].loc[datasets['Image Index'].iloc[i]] = datasets['Dataset'].iloc[i]
-    it += 1
-    if it > 15:
-        break
 
 new_df.to_csv(PATH + "organized_dataset.csv")
-print(new_df.head())
