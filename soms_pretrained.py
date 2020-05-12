1442368a225b972677ae36debf2a561a51dae947
Gustavo Beck <teodoro.beck@gmail.com>
2020-05-12 20:31:23 +0200

    SOMs pretrained working, we need to improve the visualization tough

--- soms_pretrained.py
+++ soms_pretrained.py
@@ -56,91 +56,124 @@ class SOM:
             for i in tqdm(range(0, inputs.shape[0], batch_size)):
                 batch = inputs[i:i+batch_size]
                 self.update_grid(batch, lr, current_radious)
-                
 
-dictionary = {0:'Atelectasis', 1:'Cardiomegaly', 2:'Consolidation', 3:'Edema', 4:'Effusion', 5:'Emphysema', 6:'Fibrosis', 7:'Hernia', 8:'Infiltration', 9:'Mass', 10:'COVID-19', 11:'Nodule', 12:'Pleural_Thickening', 13:'Pneumonia', 14:'Pneumothorax'}      
-path_for_soms = r'C:\Users\teodo\OneDrive\Desktop\KTH\4th_Period\Deep_Learning\Project\DD2424-Deep_Learning-COVID-Project\pytorch_example\balanced_data.pickle'
-soms_dataset = pickle.load(open(path_for_soms, 'rb'))
-indexes = np.random.permutation(soms_dataset.labels.shape[0])
-soms_dataset.data = soms_dataset.data[indexes]
-soms_dataset.labels = soms_dataset.labels[indexes]
+# Reading Covid
+path2covid = 'covid_dataset/'
+df_covid = pd.read_csv(path2covid + "metadata.csv")
+df_covid = df_covid[(df_covid['modality'] == "X-ray") & (df_covid['view'] != "L") & (df_covid['finding'] == "COVID-19")]
+df_covid = df_covid[['finding', 'filename']]
+covid_list = df_covid['finding'].values
+covid_image_list = df_covid['filename'].values
+amount_of_covid = len(df_covid)
+path2covid_images = 'covid_dataset/images/'
 
+# Read the other diseases dataset
 df = pd.read_csv("xray_dataset/organized_dataset.csv")
 df.pop('No Finding')  # Deleting the column no findings
 cols = np.array(df.columns)
-image_list = ['00000001_000.png',
-        '00000040_001.png',
-        '00000011_000.png',
-        '00000111_000.png',
-        '00000005_006.png', 
-        '00000061_001.png',
-        '00000008_002.png',
-        '00000011_006.png',
-        '00000071_001.png',
-        '00000020_000.png',
-        '00000061_015.png',
-        '00000022_001.png',
-        '00000032_024.png',
-        '00030759_000.png']
-model_path = 'final_model_2.pt'
+diseases_list = cols[2:]
+df.loc[:,'sum'] = df[cols[2:]].sum(axis=1)
+df = df[df['sum'] == 1]
+df.pop('sum')
+df.pop('Dataset')
 
-def generateMap(model_path, pathImageFile, device):
-    # Load model
-    
+image_list = []
+image_labels = []
+sample_num = amount_of_covid
+counter = np.zeros(len(diseases_list))
+for i, row in tqdm(df.iterrows()):
+    disease_index = np.argmax(list(row[1:].values))
+    disease_name = diseases_list[disease_index]
+    if counter[disease_index] < sample_num:
+        counter[disease_index] += 1
+        image_list.append(row[0])
+        image_labels.append(disease_name)
+
+image_list.extend(covid_image_list)
+image_labels.extend(covid_list)
+
+try:
+    print("Findind data")
+    data = pickle.load(open('data_som.pickle', 'rb'))
+except IOError:
+    print("Couldn't find data")
+    model_path = 'final_model_2.pt'
+
+    def generateMap(model, pathImageFile, device):
+
+        # Prepare image
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        image_transform = transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std)
+        ])
+
+        imageData = Image.open(pathImageFile).convert('RGB')
+        imageData = image_transform(imageData)
+        imageData = imageData.unsqueeze_(0)
+        imageData = imageData.to(device)
+
+        # Walk the image through the model
+        output = model(imageData)
+        #plt.imshow(output.detach().numpy().reshape((32,32)))
+        #plt.show()
+        flat_output = output.detach().numpy()
+        return flat_output
+
+    flatten = []
+    print("START WALKING THE IMAGES THROUGH THE MODEL")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model= torch.load(model_path, map_location='cpu')
     model.to(device)
-    model = model.features
+    model.classifier = torch.nn.Identity()
     model.eval() # Doesn't store gradients
+    for i in tqdm(range(len(image_list))):
+        if image_labels[i] == 'COVID-19':
+            pathInputImage = path2covid_images + image_list[i]
+        else:
+            pathInputImage = 'images_small/' + image_list[i]
+        flatten.append(generateMap(model, pathInputImage, device))
 
-    # Prepare image
-    mean = [0.485, 0.456, 0.406]
-    std = [0.229, 0.224, 0.225]
-    image_transform = transforms.Compose([
-        transforms.CenterCrop(64),
-        transforms.ToTensor(),
-        transforms.Normalize(mean, std)
-    ])
+    flattened = np.array(flatten)
+    data = myData(flattened, image_labels)
+    pickle.dump(data, open('data_som.pickle', 'wb'))
 
-    imageData = Image.open(pathImageFile).convert('RGB')
-    imageData = image_transform(imageData)
-    imageData = imageData.unsqueeze_(0)
-    imageData = imageData.to(device)
-
-    # Walk the image through the model
-    output = model(imageData)
-    output = torch.nn.functional.relu(output)
-
-    return output.reshape(-1).detach().numpy()
-flatten = []
+print("start SOMS")
 # Train SOM
-grid_height = 10
-grid_width = 10
+grid_height = 20
+grid_width = 20
 epochs = 20
 batch_size = 1
-for i in range(len(image_list)):
-    pathInputImage = 'images_small/' + image_list[i]
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    flatten.append(generateMap(model_path, pathInputImage, device))
-
-flattened = np.array(flatten)
-som = SOM(flattened, grid_width, grid_height)
-som.train(flattened, epochs, batch_size)
+# Shuffling the data
+data_flattened = data.data # array
+data_labels = data.labels # list
+shuffling_index = np.random.permutation(data_flattened.shape[0])
+data_flattened = data_flattened[shuffling_index]
+data_flattened = data_flattened.reshape(-1, data_flattened.shape[2])
+data_labels = np.array(data_labels)[shuffling_index]
+som = SOM(data_flattened, grid_width, grid_height)
+som.train(data_flattened, epochs, batch_size)
+all_diseases_labels = set(image_labels)
 
 # Plot the clusters, This is going to take much time
+print("START CLUSTERS PLOTS")
+dictionary = {0:'Atelectasis', 1:'Cardiomegaly', 2:'Consolidation', 3:'Edema', 4:'Effusion', 5:'Emphysema', 6:'Fibrosis', 7:'Hernia', 8:'Infiltration', 9:'Mass', 10:'COVID-19', 11:'Nodule', 12:'Pleural_Thickening', 13:'Pneumonia', 14:'Pneumothorax'}
 ranking = np.zeros((grid_width, grid_height, 15))
-for sample, label in tqdm(zip(flattened, soms_dataset.labels)):
+print(list(dictionary.values()).index('Cardiomegaly'))
+for sample, label in tqdm(zip(data_flattened, data_labels)):
     pos_x, pos_y = som.find_winner([sample])[0]
-    ranking[int(pos_x), int(pos_y), label] += 1
+    ranking[int(pos_x), int(pos_y), list(dictionary.values()).index(label)] += 1
 ranking_winners = np.array(np.argmax(ranking, axis=2), dtype=int)
 number_of_points = np.sum(ranking, axis=2)
 percentage_of_winner = np.max(ranking, axis=2)/number_of_points
 number_of_points /= np.sum(number_of_points)
 
-
-n = len(dictionary)
+n = len(all_diseases_labels)
 color= cm.rainbow(np.linspace(0,1,n))
 c=np.array(color)
-color_labels = np.zeros(len(dictionary))
+color_labels = np.zeros(len(all_diseases_labels))
 fig, ax = plt.subplots()
 for i, j in np.ndindex(grid_width, grid_height):
     label = dictionary[ranking_winners[i,j]]
