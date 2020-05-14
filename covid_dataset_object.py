925ad647c96516d7e19271a2aa3139e656ab6748
theo2021 <theo2021@gmail.com>
2020-05-14 18:13:54 +0300

    fixing the naming issues

--- covid_dataset_object.py
+++ covid_dataset_object.py
@@ -11,6 +11,9 @@ random_seed = 42
 # the path should include the organized_dataset.csv and an images folder with all tyhe images!!!!!
 PATH = r'/home/theodor/Git_Projects/DD2424-Deep_Learning-COVID-Project/covid_dataset/'
 data = pd.read_csv(PATH + "organized_dataset.csv")
+cl_list = list(data.columns)
+cl_list[0] = "Image Index"
+data.columns = cl_list
 data.pop('No Finding')  # Deleting the column no findings
 validation_percentage = 0.2
 test_percentage = 0.2
@@ -22,13 +25,13 @@ data_train, data_test = train_test_split(data_train, test_size=int(item_for_test
 mean = [0.485, 0.456, 0.406]
 std = [0.229, 0.224, 0.225]
 training_transforms = transforms.Compose([
-    transforms.Resize((224,224)),
     transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(0.5,0.5,0.5,0.5),
+    transforms.RandomRotation((-20, 20)),
     transforms.ToTensor(),
     transforms.Normalize(mean, std)  # Guarantee that it's in RGB, it's possible to transform to grayscale afterwards
 ])
 val_test_transforms = transforms.Compose([
-    transforms.Resize((224,224)),
     transforms.ToTensor(),
     transforms.Normalize(mean, std)
 ])
@@ -37,16 +40,16 @@ val_test_transforms = transforms.Compose([
 class XrayDataset(torch.utils.data.Dataset):
     def __init__(self, path_of_the_images, dataframe, transforms=None):
         self.path = path_of_the_images
-        self.images = dataframe['filename'].tolist()
-        self.labels = dataframe.iloc[:, 1:].values
+        self.df = dataframe
         self.transforms = transforms
 
     def __getitem__(self, index):
-        full_path = self.path + self.images[index]
+        row = self.df.iloc[index]
+        full_path = self.path + row.loc['Image Index']
         img_array = Image.open(full_path).convert('RGB')
         if self.transforms is not None:
             img_array = self.transforms(img_array)
-        aux = np.array(self.labels[index].tolist(), dtype=float)
+        aux = np.array(row.iloc[2:].values.tolist(), dtype=float)
         return img_array, aux
 
     def __len__(self):
